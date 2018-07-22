ee4786af08d701486e887937ae573a312106e1b2
wanyuchang <2and3and4and1@gmail.com>
2018-07-22 19:31:03 +0800

    Update content

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,19 @@
----
----
-//= require jquery3
-//= require popper
-//= require bootstrap
-console.log('🙃');
+var copyEmail = document.querySelector('#copyBtn');  
+copyEmail.addEventListener('click', function(event) {  
+  var emailTxt = document.querySelector('#email');  
+  var range = document.createRange();  
+  range.selectNode(emailTxt);  
+  window.getSelection().addRange(range);
+  try {  
+    var successful = document.execCommand('copy');  
+    var msg = successful ? 'successful' : 'unsuccessful';  
+    console.log('Copy email command was ' + msg);  
+    } catch(err) {  
+      console.log('Oops, unable to copy');  
+    }
+  window.getSelection().removeAllRanges();  
+});
+
+$('.carousel').carousel({
+    interval: 2400
+  })
