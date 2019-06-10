1e9a3eda526807d33be1a760d23cb4d81cbe332d
Wanyu Chang <wanyuchang@Wanyus-MacBook-Pro.local>
2019-06-10 15:56:51 +0800

    Add notification for copied success

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,19 +1,42 @@
-var copyEmail = document.querySelector('#copyBtn');  
-copyEmail.addEventListener('click', function(event) {  
-  var emailTxt = document.querySelector('#email');  
-  var range = document.createRange();  
-  range.selectNode(emailTxt);  
+var copyEmail = document.querySelector('#copyBtn');
+copyEmail.addEventListener('click', function(event) {
+  var emailTxt = document.querySelector('#email');
+  var range = document.createRange();
+  range.selectNode(emailTxt);
   window.getSelection().addRange(range);
-  try {  
-    var successful = document.execCommand('copy');  
-    var msg = successful ? 'successful' : 'unsuccessful';  
-    console.log('Copy email command was ' + msg);  
-    } catch(err) {  
-      console.log('Oops, unable to copy');  
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log(`Copy email command was ${msg}`);
+    var renderMsg = document.querySelector('#copied');
+    var background = document.querySelector('#underneath');
+    renderMsg.className += " d-block";
+    background.className += " blur";
+    } catch(err) {
+      console.log('Oops, unable to copy');
     }
-  window.getSelection().removeAllRanges();  
+  window.getSelection().removeAllRanges();
 });
 
 $('.carousel').carousel({
     interval: 2400
   })
+
+function handleClose(){
+  console.log(`close`)
+  var renderMsg = document.querySelector('#copied');
+  var background = document.querySelector('#underneath');
+  renderMsg.classList.remove("d-block");
+  background.classList.remove("blur");
+}
+
+function handleEsc(event){
+  var x = event.charCode
+  console.log(x)
+  var renderMsg = document.querySelector('#copied');
+  var background = document.querySelector('#underneath');
+  if (x === 27){
+    renderMsg.classList.remove("d-block");
+    background.classList.remove("blur");
+  }
+}
